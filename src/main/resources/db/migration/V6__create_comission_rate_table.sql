d86c56789d38fae2c63c50ca5533d4a458b4566d
Piotr Michalak <1piotr.michalak@gmail.com>
2024-08-01 14:25:47 +0200

    added indentations to V6__create_comission_rate_table.sql

--- src/main/resources/db/migration/V6__create_comission_rate_table.sql
+++ src/main/resources/db/migration/V6__create_comission_rate_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Commission_Rate (
-id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-value decimal(5,2),
-CHECK (value >= 0),
-CHECK (value <= 100)
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    value decimal(5,2),
+    CHECK (value >= 0),
+    CHECK (value <= 100)
 );
