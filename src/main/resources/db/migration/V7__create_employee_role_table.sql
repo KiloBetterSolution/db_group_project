86d2747feb617f4b6951a3ffb475ff7f25c50f18
JakubW-k <jakub.wiecek@kainos.com>
2024-08-01 14:41:54 +0200

    primary key correction

--- src/main/resources/db/migration/V7__create_employee_role_table.sql
+++ src/main/resources/db/migration/V7__create_employee_role_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Employee_Role (
-    role_id INT PRIMARY KEY NOT NULL,
-    employee_id INT PRIMARY KEY NOT NULL
+    role_id INT NOT NULL,
+    employee_id INT NOT NULL,
+    PRIMARY KEY (role_id, employee_id)
 );
