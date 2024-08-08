0a6e2eb2869e8960972b5d5d6a7213c64877578b
JakubW-k <jakub.wiecek@kainos.com>
2024-08-08 12:17:52 +0200

    linter correction2

--- src/main/java/org/example/models/Employee.java
+++ src/main/java/org/example/models/Employee.java
@@ -2,11 +2,11 @@ package org.example.models;
 
 public class Employee {
 
-    private final int id;
-    private final String name;
-    private final double salary;
-    private final String bankAccountNumber;
-    private final String nationalInsuranceNumber;
+    private int id;
+    private String name;
+    private double salary;
+    private String bankAccountNumber;
+    private String nationalInsuranceNumber;
 
     public Employee(int id, String name, double salary,
                     String bankAccountNumber,
@@ -22,19 +22,40 @@ public class Employee {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public double getSalary() {
         return salary;
     }
 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     public String getBankAccountNumber() {
         return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
     }
 
     public String getNationalInsuranceNumber() {
         return nationalInsuranceNumber;
     }
+
+    public void setNationalInsuranceNumber(String nationalInsuranceNumber) {
+        this.nationalInsuranceNumber = nationalInsuranceNumber;
+    }
+
 }
