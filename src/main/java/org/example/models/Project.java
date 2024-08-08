e09bb4f49d6121d59270e1deabb7c3f677baced1
Patryk Podlewski <patryk.podlwski@kainos.com>
2024-08-08 12:23:07 +0200

    fixing Project-model linter requirements

--- src/main/java/org/example/models/Project.java
+++ src/main/java/org/example/models/Project.java
@@ -6,16 +6,17 @@ public class Project {
     private String name;
     private double value;
     private Status status;
-    private int client_id;
-    private int teachLead_id;
+    private int clientId;
+    private int teachLeadId;
 
-    public Project(int id, int client_id, Status status, String name, double value, int teachLead_id) {
+    public Project(int id, int clientId, Status status, String name,
+                   double value, int teachLead_id) {
         this.id = id;
-        this.client_id = client_id;
+        this.clientId = clientId;
         this.status = status;
         this.name = name;
         this.value = value;
-        this.teachLead_id = teachLead_id;
+        this.teachLeadId = teachLead_id;
     }
 
     public int getId() {
@@ -26,12 +27,12 @@ public class Project {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public Status getStatus() {
+        return status;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public double getValue() {
@@ -42,28 +43,28 @@ public class Project {
         this.value = value;
     }
 
-    public Status getStatus() {
-        return status;
+    public String getName() {
+        return name;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int getClient_id() {
-        return client_id;
+    public int getClientId() {
+        return clientId;
     }
 
-    public void setClient_id(int client_id) {
-        this.client_id = client_id;
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
     }
 
-    public int getTeachLead_id() {
-        return teachLead_id;
+    public int getTeachLeadId() {
+        return teachLeadId;
     }
 
-    public void setTeachLead_id(int teachLead_id) {
-        this.teachLead_id = teachLead_id;
+    public void setTeachLeadId(int teachLeadId) {
+        this.teachLeadId = teachLeadId;
     }
 }
 
